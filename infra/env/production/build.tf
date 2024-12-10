28b11a550d95dd448bb95826d97e03c8235969e7
christopats <christopattison@gmail.com>
2024-12-10 16:37:30 +0000

    new build.tf

--- infra/env/production/build.tf
+++ infra/env/production/build.tf
@@ -1,3 +1,4 @@
+
 data "google_iam_policy" "p4sa_secretAccessor" {
   binding {
     role    = "roles/secretmanager.secretAccessor"
@@ -10,4 +11,51 @@ data "google_iam_policy" "p4sa_secretAccessor" {
 resource "google_secret_manager_secret_iam_policy" "policy" {
   secret_id   = google_secret_manager_secret.github_token_secret.secret_id
   policy_data = data.google_iam_policy.p4sa_secretAccessor.policy_data
+}
+
+resource "google_service_account" "cloudbuild_service_account" {
+  account_id = "cloudbuild"
+
+  depends_on = [google_project_service.platform]
+}
+
+resource "google_project_iam_member" "cloudbuild_roles" {
+  project = google_project.platform.id
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+  for_each = toset([
+    "roles/artifactregistry.reader",
+    "roles/artifactregistry.writer",
+    #     "roles/cloudbuild.workerPoolUser",
+    #     "roles/cloudsql.client",
+    "roles/firebase.admin",
+    "roles/iam.serviceAccountUser",
+    "roles/logging.logWriter",
+    "roles/run.developer",
+    "roles/secretmanager.admin"
+  ])
+  role = each.value
+
+  depends_on = [
+    google_project_service.platform,
+    google_artifact_registry_repository.platform
+  ]
+}
+
+resource "google_cloudbuild_trigger" "deploy" {
+  name            = "deploy"
+  location        = var.build_region
+  service_account = google_service_account.cloudbuild_service_account.id
+  filename        = "cloudbuild.deploy.yaml"
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.platform.id
+
+    push {
+      branch = "master"
+    }
+  }
+
+  depends_on = [
+    google_project_iam_member.cloudbuild_roles
+  ]
 }
